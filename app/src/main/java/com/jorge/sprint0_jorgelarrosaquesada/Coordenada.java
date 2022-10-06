77c8256a40fd2fcac5d1344e54ea67beeb9ac61d
Jorgeew <jorgelarrosaquesada@gmail.com>
2022-10-06 16:30:24 +0200

    first commit

---
+++ app/src/main/java/com/jorge/sprint0_jorgelarrosaquesada/Coordenada.java
@@ -0,0 +1,30 @@
+package com.jorge.sprint0_jorgelarrosaquesada;
+
+public class Coordenada {
+    private float x;
+    private float y;
+
+    public Coordenada(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Coordenada() {
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
---
+++ app/src/main/java/com/jorge/sprint0_jorgelarrosaquesada/Medida.java
@@ -0,0 +1,50 @@
+package com.jorge.sprint0_jorgelarrosaquesada;
+
+public class Medida {
+    private float valor;
+    private String tiempo;
+    private String nombre_sensor;
+    private Coordenada coordenada;
+
+    public Medida(String id, float valor, String tiempo, String nombre_sensor, Coordenada coordenada) {
+        this.valor = valor;
+        this.tiempo = tiempo;
+        this.nombre_sensor = nombre_sensor;
+        this.coordenada = coordenada;
+    }
+
+    public Medida() {
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public String getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(String tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getNombre_sensor() {
+        return nombre_sensor;
+    }
+
+    public void setNombre_sensor(String nombre_sensor) {
+        this.nombre_sensor = nombre_sensor;
+    }
+
+    public Coordenada getCoordenada() {
+        return coordenada;
+    }
+
+    public void setCoordenada(Coordenada coordenada) {
+        this.coordenada = coordenada;
+    }
+}
